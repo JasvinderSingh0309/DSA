e560c5c0e456bfc2e540a72a160c344ae76a406d
JasvinderSingh0309 <147367166+JasvinderSingh0309@users.noreply.github.com>
2024-02-07 12:34:12 +0530

    Update 05-2recursion.js

--- 05-2recursion.js
+++ 05-2recursion.js
@@ -1,1 +1,16 @@
 // return array of n fibonacci numbers using recursion.
+function fibo(n) {
+  if(n <= 1) {
+    return n;
+  }else{
+    return fibo(n-2) + fibo(n-1);
+  }
+}
+function getFibo(n) {
+  let arr = [];
+  for(let i=0;i<n;i++) {
+    arr.push(fibo(i));
+  }
+  return arr;
+}
+console.log(getFibo(8));
